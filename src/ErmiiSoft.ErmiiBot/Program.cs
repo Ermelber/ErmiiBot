dcc6f36a12a1bd1e12ade06b21f464b8bf596f87
Ermelber <ermelber@live.it>
2024-07-12 15:02:13 +0200

    Implement Discord bot base

--- src/ErmiiSoft.ErmiiBot/Program.cs
+++ src/ErmiiSoft.ErmiiBot/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+string? token = Environment.GetEnvironmentVariable("DiscordBotToken");
+
+if (string.IsNullOrEmpty(token))
+{
+    Console.WriteLine("Please provide a bot token inside the \"DiscordBotToken\" environment variable.");
+    return;
+}
+
+var client = new DiscordSocketClient();
+
+client.Log += async (logMessage) =>
+{
+    Console.WriteLine($"[{DateTime.Now}] {logMessage.Severity}: {logMessage.Message}");
+};
+
+await client.LoginAsync(TokenType.Bot, token);
+await client.StartAsync();
+await Task.Delay(Timeout.Infinite);
